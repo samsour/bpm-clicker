4ed552c7420a2a0072da3f98a270217fbbddd852
samsour <sam.sauer@hotmail.de>
2018-04-11 00:55:56 +0200

    broken but funny game

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var clicks = 0,
 totalBeats = 0,
+lastBpm = 0,
 lastClicked = (new Date()).getTime();
 
 
@@ -10,8 +11,15 @@ function bpmClicker() {
     var bpm = Math.round(60 /(timeNow - lastClicked) * 1000);
     
     totalBeats += bpm;
-
     lastClicked = timeNow;
+    
+    if(difference(lastBpm, bpm) > 10) {
+        clicks = 0;
+        totalBeats = 0;
+    }
 
-    document.getElementById('bpmContainer').value = Math.round(totalBeats/clicks) + " BPM";
+    lastBpm = bpm;
+    document.getElementById('bpmContainer').value = Math.round(totalBeats) + " BPM";
 }
+
+function difference(a, b) { return Math.abs(a - b); }
